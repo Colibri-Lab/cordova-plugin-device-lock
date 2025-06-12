6f5578a1d9ab9c830c0bfdf92802ea604f059904
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2025-06-13 00:45:53 +0400

    -

--- www/device_lock.js
+++ www/device_lock.js
@@ -1,7 +1,5 @@
 var exec = require('cordova/exec');
 
-module.exports = {
-    watch: function (success, error) {
-        exec(success, error, "DeviceLockPlugin", "watch", []);
-    }
+exports.watch = function (success, error) {
+    exec(success, error, "DeviceLockPlugin", "watch", []);
 };
