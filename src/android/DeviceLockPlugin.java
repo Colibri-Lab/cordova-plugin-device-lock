0c180b60f8d8ab8945deeef2a5abf5e94771ce0c
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2025-06-13 01:13:08 +0400

    -

--- src/android/DeviceLockPlugin.java
+++ src/android/DeviceLockPlugin.java
@@ -15,15 +15,38 @@ public class DeviceLockPlugin extends CordovaPlugin {
     private BroadcastReceiver receiver;
 
     @Override
-    protected void pluginInitialize() {
+    public boolean execute(String action, JSONArray args, final org.apache.cordova.CallbackContext callbackContext) throws JSONException {
+        if ("watch".equals(action)) {
+            startWatching(callbackContext);
+            return true;
+        }
+        return false; // invalid action
+    }
+
+    private void startWatching(final org.apache.cordova.CallbackContext callbackContext) {
+        if (receiver != null) {
+            try {
+                cordova.getActivity().unregisterReceiver(receiver);
+            } catch (IllegalArgumentException e) {
+                // Receiver not registered
+            }
+            receiver = null;
+        }
+
         receiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
                 String action = intent.getAction();
                 if (Intent.ACTION_SCREEN_OFF.equals(action)) {
                     sendEvent("deviceLocked");
+                    PluginResult result = new PluginResult(PluginResult.Status.OK, "deviceLocked");
+                    result.setKeepCallback(true);
+                    callbackContext.sendPluginResult(result);
                 } else if (Intent.ACTION_USER_PRESENT.equals(action)) {
                     sendEvent("deviceUnlocked");
+                    PluginResult result = new PluginResult(PluginResult.Status.OK, "deviceUnlocked");
+                    result.setKeepCallback(true);
+                    callbackContext.sendPluginResult(result);
                 }
             }
         };
@@ -32,6 +55,12 @@ public class DeviceLockPlugin extends CordovaPlugin {
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         filter.addAction(Intent.ACTION_USER_PRESENT);
         cordova.getActivity().registerReceiver(receiver, filter);
+
+        // Сообщаем, что watch запущен и оставляем callback активным
+        PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+        result.setKeepCallback(true);
+        callbackContext.sendPluginResult(result);
+        
     }
 
     private void sendEvent(String eventName) {
